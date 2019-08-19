275a978f3a668a3dcb9c7f4b98e9aafe54551fc6
Masatoshi Tada <rosso.e.nero84@gmail.com>
2019-08-19 17:14:22 +0900

    いろいろ

---
+++ ui/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+docker container stop ui
+mvn clean package
+docker container rm ui
+docker image rm mtada/ui:0.0.1
+docker image build -t mtada/ui:0.0.1 .
+#docker container create --name ui -p 8080:8080 mtada/ui:0.0.1
+#docker container start ui
+#open -a "/Applications/Google Chrome.app" http://localhost:8080/
+docker image push mtada/ui:0.0.1
