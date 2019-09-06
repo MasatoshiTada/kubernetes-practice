46a3b6e9d8e803dc8a3af91c0fbeb7bb8750c7a9
Masatoshi Tada <rosso.e.nero84@gmail.com>
2019-09-06 10:27:50 +0900

    色々更新

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -1,7 +1,13 @@
 package com.example.api;
 
+import com.example.api.service.CustomerService;
+import com.example.api.web.filter.LoggingFilter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +16,22 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean loggingFilter() {
+        LoggingFilter loggingFilter = new LoggingFilter();
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>(loggingFilter);
+        // フィルターの順番を一番最初に指定
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        // url-patternを指定
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public CommandLineRunner showEnv(DataSourceProperties dataSourceProperties, CustomerService customerService) {
+        return args -> {
+            System.out.println("■" + dataSourceProperties.getUrl());
+            customerService.findAll().forEach(System.out::println);
+        };
+    }
 }
--- ui/src/main/java/com/example/ui/UiApplication.java
+++ ui/src/main/java/com/example/ui/UiApplication.java
@@ -1,14 +1,21 @@
 package com.example.ui;
 
+import com.example.ui.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApiProperties.class)
+@EnableScheduling
 public class UiApplication {
 
     public static void main(String[] args) {
@@ -18,5 +25,14 @@ public class UiApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Autowired
+    CustomerService customerService;
+
+    @Scheduled(fixedDelay = 10000)
+    public void getCustomers() {
+        System.out.println("■" + LocalDateTime.now());
+        customerService.findAll().forEach(System.out::println);
     }
 }
